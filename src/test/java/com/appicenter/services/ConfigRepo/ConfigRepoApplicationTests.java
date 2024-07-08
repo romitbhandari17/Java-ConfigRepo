3e7f0775344ebeb9609e09efe1bb15753830a25b
Romit Bhandari <romit@qs.com>
2024-07-08 15:50:59 +0530

    Config repo

---
+++ src/test/java/com/appicenter/services/ConfigRepo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appicenter.services.ConfigRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
